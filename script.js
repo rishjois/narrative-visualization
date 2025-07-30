02eb3b83a2703e108c8f4b009792e7ed318576b6
rishthefishh <rishjoisGithub@gmail.com>
2025-07-29 21:02:59 -0400

    Parsed data in csv

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 async function init() {
-    d3.csvParse("nvidia.csv").then(function() {
-        console.log("data: " + data);
+    d3.csv("https://raw.githubusercontent.com/rishjois/narrative-visualization/refs/heads/main/nvidia.csv").then(function(data) {
+        console.log("data: " + data[0]);
+
+        data.forEach(row => {
+            console.log(`Name: ${row.Date}, Age: ${row.Open}, City: ${row.Close}`);
+          });
     });
 };
-  
+
+
